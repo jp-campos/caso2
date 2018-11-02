ebbee8af495af60db48d067d59c3841f0ef9c398
jp.campos <jp.campos@ISC407.sis.virtual.uniandes.edu.co>
2018-11-02 10:05:59 -0500

    Sin seguridad sirve

--- caso2/src/caso2/Cliente.java
+++ caso2/src/caso2/Cliente.java
@@ -9,6 +9,8 @@ import java.security.cert.X509Certificate;
 
 import javax.crypto.Mac;
 import javax.xml.bind.DatatypeConverter;
+
+import org.bouncycastle.asn1.crmf.CertId;
 public class Cliente extends Thread{
 
 	private static final String HOST = "127.0.0.1";
@@ -20,7 +22,7 @@ public class Cliente extends Thread{
 	private BufferedReader lector = null;
 	private Encriptar encrip; 
 	private BufferedReader stdIn = null;
-	
+
 	private Monitor monitor;
 
 
@@ -31,11 +33,11 @@ public class Cliente extends Thread{
 		sock = new Socket(HOST, PUERTO);
 		escritor = new PrintWriter(sock.getOutputStream(), true);
 		lector = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-		BufferedReader stdIn = new BufferedReader(
+		stdIn = new BufferedReader(
 				new InputStreamReader(System.in));
-		
+
 		monitor = new Monitor(); 
-		
+
 		int estado = 0; 
 		while(ejecutar)
 		{
@@ -46,11 +48,11 @@ public class Cliente extends Thread{
 				System.out.println("Escriba NOSEGURO si quiere comunicarse con un servidor sin seguridad \n SEGURO si desea hacerlo con seguridad");
 
 				String respuesta = stdIn.readLine(); 
-
+				//TODO Cambiar por el parametro despues de probar
 				if(respuesta.equals("NOSEGURO"))
-				{
+				{	
 					protocoloSinSeguridad(); 
-					
+
 				}else if(respuesta.equals("SEGURO"))
 				{
 					protocoloConSeguridad();
@@ -78,12 +80,8 @@ public class Cliente extends Thread{
 				if (estado == 0) {
 					System.out.print("Escriba el mensaje para enviar:");
 					fromUser = stdIn.readLine();
-					if(fromUser.equals("HOLA")){
-						escritor.println(fromUser);
-					}else{
-						System.out.println("No va de acuerdo con el protocolo");
-						ejecutar = false;
-					}
+					escritor.println(fromUser);
+
 					estado++; 
 				}else if(estado == 1 )
 				{
@@ -92,7 +90,7 @@ public class Cliente extends Thread{
 					{
 						System.out.println("OK");
 
-						System.out.print("Escriba el mensaje para enviar:");
+						System.out.print("Escriba los algoritmos para enviar:");
 
 						//Aqui mandar:ALGORITMOS:Blowfish:RSA:HMACMD5
 
@@ -202,14 +200,134 @@ public class Cliente extends Thread{
 
 	public void protocoloSinSeguridad()
 	{
-		//TODO: Protocolo sin seguridad
+		String respuestaServer;
+		String fromUser;
+		int estado = 0;
+
+
+		try{
+			while (ejecutar) {
+				//respuestaServer = lector.readLine(); 
+				if (estado == 0) {
+					System.out.print("Escriba el mensaje para enviar:");
+					fromUser = stdIn.readLine();
+					if(fromUser.equals("HOLA")){
+						escritor.println(fromUser);
+					}else{
+						System.out.println("No va de acuerdo con el protocolo");
+						ejecutar = false;
+					}
+					estado++; 
+				}else if(estado == 1 )
+				{
+					respuestaServer = lector.readLine(); 
+					if(respuestaServer.equalsIgnoreCase("OK"))
+					{
+						System.out.println("OK");
+
+						System.out.print("Escriba algoritmos para enviar:");
+
+						//Aqui mandar:ALGORITMOS:Blowfish:RSA:HMACMD5
+
+
+						fromUser = stdIn.readLine();
+
+						escritor.println(fromUser);
+
+
+
+						estado++; 
+					}else 
+					{
+						System.out.println(respuestaServer);
+						ejecutar = false;
+					}
+				}else if(estado == 2)
+				{
+					//Aqui mandar certificado
+					respuestaServer = lector.readLine(); 
+					System.out.println(respuestaServer);
+					if(respuestaServer.equalsIgnoreCase("OK"))
+					{
+						String certificadoEnString = "Certificado super seguro";
+						System.out.println("Mandar certificado cliente");
+						escritor.println(certificadoEnString);
+						estado++;
+
+					}else
+					{	
+						System.out.println(respuestaServer);
+						ejecutar= false;
+					}
+
+				}else if (estado == 3)
+				{
+					//RECIBIR CERTIFICADO
+
+					respuestaServer = lector.readLine(); 
+					System.out.println(respuestaServer);
+
+
+					if(respuestaServer.equalsIgnoreCase("OK"))
+					{
+						respuestaServer = lector.readLine(); 
+						System.out.println(respuestaServer);
+						System.out.println("Recibir certificado servidor");
+
+						escritor.println("OK");
+						
+						estado++;
+
+					}else
+					{
+						ejecutar= false;
+					}
+
+				}else if(estado == 4)
+				{
+					//Devolver la llave
+					System.out.println("Dolver la llave");
+					respuestaServer = lector.readLine(); 
+					escritor.println(respuestaServer);
+
+					estado++; 
+
+				}else if(estado == 5)
+				{
+					respuestaServer = lector.readLine(); 
+					System.out.println(respuestaServer);
+					System.out.println("Haga la consulta");
+
+					fromUser = stdIn.readLine();
+					escritor.println(fromUser);
+
+					fromUser = stdIn.readLine();
+					escritor.println(fromUser);
+
+					respuestaServer = lector.readLine(); 
+					System.out.println(respuestaServer);
+
+					ejecutar= false;
+				}
+
+			}
+			escritor.close();
+			lector.close();
+			// cierre el socket y la entrada estándar
+			stdIn.close();
+			sock.close();
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			System.exit(1);
+		}
+
 
 	}
 
 	public static void main(String[] args) throws Exception
 	{
 		Cliente cliente = new Cliente("SEGURO");    
-		
-		
+
+
 	}
 }
